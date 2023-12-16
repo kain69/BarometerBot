cf707a24a5299a46d4c169009f90f7d70e9392af
Vlad Karmazin <karmazin00@gmail.com>
2023-12-16 16:52:28 +0500

    DTO для текущей погоды

---
+++ src/main/java/ru/karmazin/barometerbot/dto/currentWeather/WindDto.java
@@ -0,0 +1,8 @@
+package ru.karmazin.barometerbot.dto.currentWeather;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WindDto {
+    private Long speed;
+    private Long deg;
+}
